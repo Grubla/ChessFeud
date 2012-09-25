da4c55ef4611fb79b699b00c2e1fb1ce5eb92ce0
Henrik Alburg <grubla@kanin1.(none)>
2012-09-25 10:19:16 +0200

    Commented the entire ChessModel

--- ChessFeud/src/se/chalmers/chessfeud/model/ChessModel.java
+++ ChessFeud/src/se/chalmers/chessfeud/model/ChessModel.java
@@ -9,13 +9,19 @@ import se.chalmers.chessfeud.model.pieces.Piece;
 import se.chalmers.chessfeud.model.pieces.Queen;
 import se.chalmers.chessfeud.model.pieces.Rook;
 import se.chalmers.chessfeud.model.utils.Position;
-
+/**
+ * A class that implements the model of a chessgame
+ * @author grubla
+ *
+ */
 public class ChessModel {
 	private ChessBoard chessBoard;
 	private int activePlayer;
 	private Position selected;
 	private List<Position> possibleMoves;
-	
+	/**
+	 * Creates an instance of chess with a new starting board. 
+	 */
 	public ChessModel(){
 		chessBoard = new ChessBoard();
 		activePlayer = 0;
@@ -23,10 +29,38 @@ public class ChessModel {
 		possibleMoves = new LinkedList<Position>();
 	}
 	
+	/**
+	 * Represents a click on the board, the first click being choosing a piece and the second where it should go
+	 * @param p, the position clicked
+	 */
 	public void click(Position p){
-		//Selected?
+		if(selected != null){ //Some piece is selected
+			if(possibleMoves.contains(p)){ //A valid move has been clicked
+				//Save the newPos piece
+				//Move the piece
+				//Check for check draw etc
+				//Next turn or gameOver
+			}else{ //Clicked on a place the piece cannot go
+				if(chessBoard.getPieceAt(p).getTeam() == activePlayer){ 
+					if(p.equals(selected)){ //Clicked on the same piece twice
+						deselectPiece();
+					}else{ //Clicked on a new piece in its team
+						setSelected(p);
+					}
+				}else{ //Clicked on an enemy piece where it cannot go
+					deselectPiece();
+				}
+			}
+		}else{ 
+			if(chessBoard.getPieceAt(p).getTeam() == activePlayer){ //Clicked on a new valid piece
+				setSelected(p);
+			}
+		}
 	}
-	
+	/*
+	 * Returns the possible moves (according to ALL rules) for the chosen piece.
+	 * @param pos, the position of the chosen piece
+	 */
 	private void getPossibleMoves(Position pos){
 		possibleMoves.clear();
 		Piece piece = chessBoard.getPieceAt(pos);
@@ -47,15 +81,17 @@ public class ChessModel {
 		}
 		
 	}
-	
+	/* Returns true if the pieces at the given positions are on the same team */
 	private boolean sameTeam(Position pos, Position p) {
 		return chessBoard.getPieceAt(pos).getTeam() == chessBoard.getPieceAt(p).getTeam();
 	}
 
+	/* Returns true if there is no piece at the given position */
 	private boolean isEmpty(Position pos) {
 		return chessBoard.getPieceAt(pos) == null;
 	}
 
+	/* Returns true if the selected position -> pos results in a state that is not check */
 	private boolean notCheck(Position pos){
 		ChessBoard tmpBoard = new ChessBoard(chessBoard, selected, pos);
 		for(int x = 0; x < tmpBoard.width(); x++)
@@ -85,25 +121,37 @@ public class ChessModel {
 		return true;
 	}
 	
+	/**
+	 * Returns the active player where 0 is "white" and 1 is "black"
+	 * @return the player who's turn it is to move
+	 */
 	public int acitvePlayer(){
 		return activePlayer;
 	}
 	
+	/**
+	 * Returns the piece at the given position
+	 * @param p, a position on the board 0 <= x,y < 8 
+	 * @return a Piece object
+	 */
 	public Piece getPieceAt(Position p){
 		return null;
 	}
 	
-	
+	/* Sets the current position as the selected one */
 	private void setSelected(Position p){
 		selected = p;
+		getPossibleMoves(selected);
 	}
 	
+	/* Deselcts the current selected piece */
+	private void deselectPiece(){
+		selected = null;
+	}
+	
+	/* Changes the turn (the actve player) */
 	private void changeTurn(){
 		activePlayer = (activePlayer+1)%2;
-	}
-
-	private boolean tryMove(Position oldPos, Position newPos){
-		return false;
 	}
 	
 }
