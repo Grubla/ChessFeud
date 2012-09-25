1d3796bebecbc39ef5381b5ce8657a24e399e54d
Shortleft <arvid.b.karlsson@gmail.com>
2012-09-25 10:48:38 +0200

    Added a constants class

---
+++ ChessFeud/src/se/chalmers/chessfeud/constants/C.java
@@ -0,0 +1,12 @@
+package se.chalmers.chessfeud.constants;
+
+public class C {
+
+	public static final int PIECE_PAWN = 0;
+	public static final int PIECE_ROOK = 1;
+	public static final int PIECE_BISHOP = 2;
+	public static final int PIECE_KNIGHT = 3;
+	public static final int PIECE_QUEEN = 4;
+	public static final int PIECE_KING = 5;
+	
+}
