bee9e39a74e8c9db79389beb5b7b44f8a91bb698
Sean Pavlov <sean.pavlov@gmail.com>
2012-09-26 10:22:58 +0200

    Continuing on GameView, need to fetch the Constant package

--- ChessFeud/src/se/chalmers/chessfeud/view/GameView.java
+++ ChessFeud/src/se/chalmers/chessfeud/view/GameView.java
@@ -3,12 +3,14 @@ package se.chalmers.chessfeud.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GameView extends View{
+	
 
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -20,8 +22,14 @@ public class GameView extends View{
 		super.onDraw(canvas);
 		int chessSquareHeight = this.getHeight()/8;
 		int chessSquareWidth = this.getWidth()/8;
+		
+		//White square property
 		Paint pW = new Paint();
-		pW.setColor(25);
+		Color cW = new Color();
+		Color.rgb(11, 11, 11);
+		pW.setColor(new Color(11,11,11));
+		
+		//Black square property
 		Paint pB = new Paint();
 		//En till loop
 		for(int i = 0; i < 8; i++){
