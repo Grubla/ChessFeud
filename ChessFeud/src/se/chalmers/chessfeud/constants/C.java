2bdeb38316da1df56bf9b5032e6f0285b291fa26
Sean Pavlov <sean.pavlov@gmail.com>
2012-09-28 15:15:10 +0200

    Deleted project and cloned it again to solve git problem. Updated GameView so it is possible to see which piece is selected and its possible moves

--- ChessFeud/src/se/chalmers/chessfeud/constants/C.java
+++ ChessFeud/src/se/chalmers/chessfeud/constants/C.java
@@ -10,7 +10,11 @@ public class C {
 	public static final int PIECE_KING = 5;
 	
 	public static final int SQUARE_WHITE = 0xFFFFCE9E;
+	public static final int SQUARE_WHITE_SELECTED = 0xFFFFDE5F;
+	public static final int SQUARE_WHITE_POSSIBLE_MOVES = 0xFFAAE25F;
 	public static final int SQUARE_BLACK = 0xFFD18B47;
+	public static final int SQUARE_BLACK_SELECTED = 0xFFE3B62B;
+	public static final int SQUARE_BLACK_POSSIBLE_MOVES = 0xFF8EB92B;
 	public static final int TEAM_WHITE = 0;
 	public static final int TEAM_BLACK = 1;
 }
--- ChessFeud/src/se/chalmers/chessfeud/view/GameView.java
+++ ChessFeud/src/se/chalmers/chessfeud/view/GameView.java
@@ -1,5 +1,7 @@
 package se.chalmers.chessfeud.view;
 
+import java.util.List;
+
 import se.chalmers.chessfeud.constants.C;
 import se.chalmers.chessfeud.model.ChessModel;
 import se.chalmers.chessfeud.model.pieces.NoPiece;
@@ -55,49 +57,76 @@ public class GameView extends View implements OnTouchListener{
 
 		chessSquareHeight = this.getHeight() / 8;
 		chessSquareWidth = this.getWidth() / 8;
-		// White square property
-		Paint pW = new Paint();
-		pW.setColor(C.SQUARE_WHITE);
+		// White square main
+				Paint wMain = new Paint();
+				wMain.setColor(C.SQUARE_WHITE);
+				// White square selected
+				Paint wSelected = new Paint();
+				wSelected.setColor(C.SQUARE_WHITE_SELECTED);
+				// White square available
+				Paint wAvailable = new Paint();
+				wAvailable.setColor(C.SQUARE_WHITE_POSSIBLE_MOVES);
 
-		// Black square property
-		Paint pB = new Paint();
-		pB.setColor(C.SQUARE_BLACK);
+				// Black square main
+				Paint bMain = new Paint();
+				bMain.setColor(C.SQUARE_BLACK);
+				// Black square selected
+				Paint bSelected = new Paint();
+				bSelected.setColor(C.SQUARE_BLACK_SELECTED);
+				// Black square available
+				Paint bAvailable = new Paint();
+				bAvailable.setColor(C.SQUARE_BLACK_POSSIBLE_MOVES);
 
-		// Draw every square
-		boolean paintWhite = true;
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				Rect r = new Rect(j * chessSquareWidth, i * chessSquareHeight,
-						(j + 1) * chessSquareWidth, (i + 1) * chessSquareWidth);
-				if (paintWhite) {
-					canvas.drawRect(r, pW);
-				} else {
-					canvas.drawRect(r, pB);
+				List<Position> possibleSquares = gm.getPossibleMoves();
+				// Draw every square
+				boolean paintWhite = true;
+				for (int y = 0; y < 8; y++) {
+					for (int x = 0; x < 8; x++) {
+						Rect r = new Rect(x * chessSquareWidth, y * chessSquareHeight,
+								(x + 1) * chessSquareWidth, (y + 1) * chessSquareWidth);
+						
+						if (paintWhite) {
+							if(possibleSquares.contains(new Position(x, y))){
+								canvas.drawRect(r, wAvailable);
+							}else if(new Position(x, y).equals(gm.getSelectedPosition())){
+								canvas.drawRect(r, wSelected);
+							}else{
+								canvas.drawRect(r, wMain);
+							}
+							
+						} else {
+							if(possibleSquares.contains(new Position(x, y))){
+								canvas.drawRect(r, bAvailable);
+							}else if(new Position(x, y).equals(gm.getSelectedPosition())){
+								canvas.drawRect(r, bSelected);
+							}else{
+								canvas.drawRect(r, bMain);
+							}
+						}
+
+						Piece p = gm.getPieceAt(new Position(x, y));
+						p.toString();
+						// Paint piece at position is there is any
+						if (!(p instanceof NoPiece)) {
+							String uri = getPieceFileName(p.getTeam(), p.getId());
+							uri = "drawable/pieces_" + uri;
+							int imageResource = getResources().getIdentifier(uri, null,
+									context.getPackageName());
+							Drawable image = getResources().getDrawable(imageResource);
+							Bitmap bm = BitmapFactory.decodeResource(getResources(),
+									imageResource);
+							bm = Bitmap.createScaledBitmap(bm, chessSquareWidth,
+									chessSquareHeight, false);
+							canvas.drawBitmap(bm, x * chessSquareWidth, y
+									* chessSquareHeight, null);
+						}
+						// Change the color that is going to be painted for each square
+						paintWhite = !paintWhite;
+					}
+					// Change the color after each row so the board i checked.
+					paintWhite = !paintWhite;
 				}
-
-				Piece p = gm.getPieceAt(new Position(j, i));
-				p.toString();
-				// Paint piece at position is there is any
-				if (!(p instanceof NoPiece)) {
-					String uri = getPieceFileName(p.getTeam(), p.getId());
-					uri = "drawable/pieces_" + uri;
-					int imageResource = getResources().getIdentifier(uri, null,
-							context.getPackageName());
-					Drawable image = getResources().getDrawable(imageResource);
-					Bitmap bm = BitmapFactory.decodeResource(getResources(),
-							imageResource);
-					bm = Bitmap.createScaledBitmap(bm, chessSquareWidth,
-							chessSquareHeight, false);
-					canvas.drawBitmap(bm, j * chessSquareWidth, i
-							* chessSquareHeight, null);
-				}
-				// Change the color that is going to be painted for each square
-				paintWhite = !paintWhite;
 			}
-			// Change the color after each row so the board i checked.
-			paintWhite = !paintWhite;
-		}
-	}
 
 	/**
 	 * Returning the filename of a certain piece without directory and file
