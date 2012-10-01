8f89aae54e506e44ecc9255e8e92f299f1ba727a
Shortleft <arvid.b.karlsson@gmail.com>
2012-10-01 16:09:25 +0200

    Created a test environment for androidn using jUnit

---
+++ ChessFeudTest/src/se/chalmers/chessfeud/test/modelTest.java
@@ -0,0 +1,21 @@
+package se.chalmers.chessfeud.test;
+
+import org.junit.After;
+
+import org.junit.Before;
+
+import android.test.AndroidTestCase;
+
+public class modelTest extends AndroidTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+}
