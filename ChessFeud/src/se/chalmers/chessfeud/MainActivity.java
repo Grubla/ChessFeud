070d6bd47fb4e82b6b4ece0745071e7b2989ea66
Henrik Alburg <henrik.alburg@gmail.com>
2012-10-02 22:04:33 +0200

    Started on the adapter for the gameslist

--- ChessFeud/src/se/chalmers/chessfeud/MainActivity.java
+++ ChessFeud/src/se/chalmers/chessfeud/MainActivity.java
@@ -1,16 +1,26 @@
 package se.chalmers.chessfeud;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Button bPlay, bMyProfile, bSettings, bAbout;
+	private ImageView iLogo;
+	private ListView finishedGames, startedGames;
 	
 
     @Override
@@ -23,6 +33,20 @@ public class MainActivity extends Activity implements OnClickListener{
         bMyProfile.setOnClickListener(this);
         bSettings.setOnClickListener(this);
         
+        iLogo = (ImageView) findViewById(R.id.imageView1);
+        iLogo.setOnClickListener(this);
+        
+        finishedGames = (ListView) findViewById(R.id.list_finishedGames);
+        startedGames = (ListView) findViewById(R.id.list_ongoingGames);
+        
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	
+    	//finishedGames.setListAdapter(this, R.id.list_finishedGames, getList());
+    	//startedGames.setListAdapter(this, R.id.list_ongoingGames, getList());
     }
 
     @Override
@@ -44,5 +68,32 @@ public class MainActivity extends Activity implements OnClickListener{
     	default:
     		Log.d("Default", "Should not get here!");
     	}
+    }
+    
+    private class GameListAdapter extends ArrayAdapter<String> {
+    	private Context context;
+    	private int resourceId;
+    	
+    	public GameListAdapter(Context context, int resId, List<String> l){
+    		super(context, resId, new String[] {"hej", "lol"});
+    		this.context = context;
+    		this.resourceId = resId;
+    		//To be fixed here when the view is finsihed
+    	}
+    	
+    	@Override
+    	public View getView(int position, View convertView, ViewGroup parent) {
+
+    		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    		View view = inflater.inflate(resourceId, parent, false);
+    		//TextView opponentName = (TextView) view.findViewById(123534)
+    		//Do this for every element
+    		//String s = l.get(position);
+    		//decode s
+    		//opponentView.setText("decoded s")
+    			
+    			return view;
+    	}
+    	
     }
 }
