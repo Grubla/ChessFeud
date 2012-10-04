0b39674e4228e66b4d8431a14692b868347cb053
Henrik Alburg <grubla@kanin1.(none)>
2012-10-04 17:03:10 +0200

    Added the positions for a new gametest

--- ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/modelTest.java
+++ ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/modelTest.java
@@ -53,4 +53,52 @@ public class modelTest extends AndroidTestCase {
 		}
 	}
 	
+	public void testGame2(){
+		Position[] pos = {
+				// Click 			White Move			Click			Black move
+				new Position(6,7),new Position(5,5),new Position(7,1),new Position(7,3),
+				new Position(5,5),new Position(6,3),new Position(6,1),new Position(6,2),
+				new Position(4,6),new Position(4,5),new Position(5,1),new Position(5,3),
+				new Position(5,7),new Position(2,4),new Position(4,1),new Position(4,2),
+				new Position(5,6),new Position(5,4),new Position(3,1),new Position(3,3),
+				new Position(2,4),new Position(1,3)/*Check*/,new Position(2,1),new Position(2,2),
+				new Position(1,3),new Position(5,7),new Position(1,1),new Position(1,3),
+				new Position(3,6),new Position(3,4),new Position(0,1),new Position(0,2),
+				new Position(6,3),new Position(5,5),new Position(0,5),new Position(1,4),/*Check*/
+				new Position(2,7),new Position(3,6),new Position(2,2),new Position(2,3),
+				new Position(5,5),new Position(4,3),new Position(6,0),new Position(4,1),
+				new Position(3,4),new Position(2,3),new Position(1,4),new Position(2,3),
+				new Position(1,6),new Position(1,4),new Position(2,3),new Position(1,2),
+				new Position(3,6),new Position(2,5),new Position(1,2),new Position(4,5),
+				new Position(3,7),new Position(5,5),new Position(4,5),new Position(2,7),
+				new Position(4,3),new Position(3,5),new Position(3,3),new Position(3,4),
+				new Position(3,5),new Position(2,7),new Position(3,4),new Position(2,5),
+				new Position(5,5),new Position(0,0),new Position(4,1),new Position(2,2),
+				new Position(1,7),new Position(2,5),new Position(3,0),new Position(3,4),
+				new Position(2,7),new Position(4,6),new Position(3,4),new Position(1,4),
+				new Position(0,7),new Position(1,7),new Position(1,4),new Position(0,5),
+				new Position(6,6),new Position(6,5),new Position(7,0),new Position(7,1),
+				new Position(5,7),new Position(6,6),new Position(2,0),new Position(1,1),
+				new Position(6,1),new Position(2,2),new Position(1,1),new Position(2,2),
+				new Position(0,0),new Position(1,0),new Position(4,0),new Position(5,1),
+				new Position(1,0),new Position(2,1),new Position(5,1),new Position(6,0),
+				new Position(2,1),new Position(2,2),new Position(0,5),new Position(4,1),
+				new Position(2,2),new Position(0,2),new Position(1,3),new Position(1,4),
+				new Position(0,2),new Position(2,0),new Position(4,1),new Position(5,0),
+				new Position(2,0),new Position(4,2),new Position(7,1),new Position(5,1),
+				new Position(4,2),new Position(6,2),new Position(5,1),new Position(6,1),
+				new Position(6,2),new Position(4,2),new Position(6,1),new Position(5,1),
+				new Position(1,7),new Position(1,4),new Position(5,0),new Position(1,4),
+				new Position(4,2),new Position(6,2),new Position(5,1),new Position(6,1),
+				new Position(6,2),new Position(5,3),new Position(1,4),new Position(2,5),
+				new Position(4,6),new Position(2,5),new Position(6,1),new Position(4,1),
+				new Position(2,5),new Position(4,6),new Position(4,1),new Position(4,6),
+				new Position(4,7),new Position(4,6),new Position(6,0),new Position(6,1),
+				new Position(7,7),new Position(3,7),new Position(6,1),new Position(7,3),
+				new Position(3,7),new Position(3,2),new Position(7,2),new Position(6,1),
+				new Position(5,3),new Position(7,3),new Position(6,1),new Position(5,0),
+				new Position(3,2),new Position(3,1),new Position(5,0),new Position(6,0),
+				new Position(7,3),new Position(4,0)/*CHECKMATE*/};
+	}
+	
 }
