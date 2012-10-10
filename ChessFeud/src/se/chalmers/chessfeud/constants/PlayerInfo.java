5af7b847fc33015fea0fb455fd382c599665aa3f
twister <simonwalmgren@gmail.com>
2012-10-10 14:16:22 +0200

    added a new class for handling the current logged in players credentials, it saves the info to a local file for fast inlog

---
+++ ChessFeud/src/se/chalmers/chessfeud/constants/PlayerInfo.java
@@ -0,0 +1,56 @@
+package se.chalmers.chessfeud.constants;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import android.os.Environment;
+
+public class PlayerInfo {
+	
+	private static PlayerInfo instance = null;
+	
+	private String userName;
+	private String password;
+
+	
+	protected PlayerInfo() {
+	}
+	
+	public static PlayerInfo getInstance() {
+		if(instance == null) {
+			return new PlayerInfo();
+		}
+		return instance;
+	}
+	
+	private String getUserName() {
+		;
+	}
+	
+	private String getPassWord() {
+		;
+	}
+	private void loadInfoFromFile() {
+		File sdcard = Environment.getExternalStorageDirectory();
+		File credentials = new File(sdcard, "usercred.txt");
+		StringBuilder info = new StringBuilder();
+		
+		try {
+		    BufferedReader br = new BufferedReader(new FileReader(credentials));
+		    String line;
+
+		    while ((line = br.readLine()) != null) {
+		        info.append(line);
+		        info.append('\n');
+		    }
+		}
+		catch (IOException e) {
+		}
+
+		
+	}
+
+
+}
