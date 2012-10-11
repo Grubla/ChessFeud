6af298523d43388a58fcfdf41e4f7cca511c595d
Shortleft <arvid.b.karlsson@gmail.com>
2012-10-11 15:05:35 +0200

    Changed the Bishop Test to use equals

--- ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/bishopTest.java
+++ ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/bishopTest.java
@@ -7,7 +7,7 @@ import se.chalmers.chessfeud.model.pieces.Piece;
 import se.chalmers.chessfeud.model.utils.Position;
 import android.test.AndroidTestCase;
 
-public class bishopTest extends AndroidTestCase {
+public class BishopTest extends AndroidTestCase {
 
 	public void testBishop() {
 		Piece bishop = new Bishop(0);
@@ -17,10 +17,10 @@ public class bishopTest extends AndroidTestCase {
 			for (List<Position> l : testList) {
 				for (Position p : l) {
 					if (i == 0) {
-						assertTrue(p != new Position(8, 8));
-						assertTrue(p != new Position(7, 7));
-						assertTrue(p != new Position(7, 8));
-						assertTrue(p != new Position(6, 8));
+						assertTrue(!p.equals(new Position(8, 8)));
+						assertTrue(!p.equals(new Position(7, 7)));
+						assertTrue(!p.equals(new Position(7, 8)));
+						assertTrue(!p.equals(new Position(6, 8)));
 						assertTrue(p.getX() >= 0 && p.getX() <= 7
 								&& p.getY() >= 0 && p.getY() <= 7
 								&& (p.getX() == p.getY()));
