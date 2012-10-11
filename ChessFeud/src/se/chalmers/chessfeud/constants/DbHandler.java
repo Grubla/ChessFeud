8f7a5eb1d6a2823453d97551faffb7f96409dd5d
twister <simonwalmgren@gmail.com>
2012-10-11 14:35:23 +0200

    added a method for ecrypting strings using the md5 encryption pattern

--- ChessFeud/src/se/chalmers/chessfeud/constants/DbHandler.java
+++ ChessFeud/src/se/chalmers/chessfeud/constants/DbHandler.java
@@ -2,8 +2,10 @@ package se.chalmers.chessfeud.constants;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -265,6 +267,18 @@ public class DbHandler {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	/**
+	 * A method for encrypting a string using the common md5-encryption pattern.
+	 * @param s
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public String encrypt(String s) throws NoSuchAlgorithmException {
+		MessageDigest md5enc = MessageDigest.getInstance("MD5");
+		md5enc.update(s.getBytes(),0,s.length());
+		String encryption = new BigInteger(1, md5enc.digest()).toString(16);
+		return encryption;
 	}
 
 }
