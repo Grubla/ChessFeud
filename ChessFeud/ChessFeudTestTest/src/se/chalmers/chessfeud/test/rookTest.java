c162a701730d4c61d953055aaa35090b5ce083c4
Shortleft <arvid.b.karlsson@gmail.com>
2012-10-11 15:04:54 +0200

    Updatet the test for the Rook. Used equals instead of ==

--- ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/rookTest.java
+++ ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/rookTest.java
@@ -7,7 +7,7 @@ import se.chalmers.chessfeud.model.pieces.Rook;
 import se.chalmers.chessfeud.model.utils.Position;
 import android.test.AndroidTestCase;
 
-public class rookTest extends AndroidTestCase {
+public class RookTest extends AndroidTestCase {
 
 	public void testRook() {
 		Piece rook = new Rook(0);
@@ -17,11 +17,11 @@ public class rookTest extends AndroidTestCase {
 			for (List<Position> l : testList) {
 				for (Position p : l) {
 					if (i == 0) {
-						assertTrue(p != new Position(8, 8));
-						assertTrue(p != new Position(7, 7));
-						assertTrue(p != new Position(7, 8));
-						assertTrue(p != new Position(6, 6));
-						assertTrue(p != new Position(6, 8));
+						assertTrue(!p.equals(new Position(8, 8)));
+						assertTrue(!p.equals(new Position(7, 7)));
+						assertTrue(!p.equals(new Position(7, 8)));
+						assertTrue(!p.equals(new Position(6, 6)));
+						assertTrue(!p.equals(new Position(6, 8)));
 						assertTrue((p.getX() >= 0 && p.getX() <= 7 && p.getY() == 7)
 								|| (p.getY() >= 0 && p.getY() <= 7 && p.getX() == 7));
 					}
@@ -30,10 +30,10 @@ public class rookTest extends AndroidTestCase {
 								|| (p.getY() >= 0 && p.getY() <= 7
 										&& p.getX() == 3 && !(p.getX() == 3 && p
 										.getY() == 3)));
-						assertTrue(p != new Position(2, 2));
-						assertTrue(p != new Position(4, 4));
-						assertTrue(p != new Position(2, 4));
-						assertTrue(p != new Position(4, 2));
+						assertTrue(!p.equals(new Position(2, 2)));
+						assertTrue(!p.equals(new Position(4, 4)));
+						assertTrue(!p.equals(new Position(2, 4)));
+						assertTrue(!p.equals(new Position(4, 2)));
 					}
 				}
 			}
