9a1f5df06d8f28ca93c1f413bc9dae671665ea4e
twister <simonwalmgren@gmail.com>
2012-10-16 14:46:55 +0200

    updated playerinfo with javadoc

--- ChessFeud/src/se/chalmers/chessfeud/constants/PlayerInfo.java
+++ ChessFeud/src/se/chalmers/chessfeud/constants/PlayerInfo.java
@@ -11,66 +11,101 @@ import java.security.NoSuchAlgorithmException;
 import android.os.Environment;
 
 public class PlayerInfo {
-	
+
 	private static PlayerInfo instance = null;
-	
+
 	private String userName;
 	private String password;
-	
+
 	private static File sdcard;
 	private static File credentials;
-	
+
 	private boolean loggedIn = false;
 
-	
+	/**
+	 * A singleton class for handling info about the player currently logged in.
+	 */
 	protected PlayerInfo() {
 		loadInfoFromFile();
 	}
-	
+
+	/**
+	 * If there isnt an instance, i creates an instance and initiates it, else
+	 * it just returns the instance.
+	 * 
+	 * @return
+	 */
 	public static PlayerInfo getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new PlayerInfo();
 			sdcard = Environment.getExternalStorageDirectory();
 			credentials = new File(sdcard, "usercred.txt");
 		}
 		return instance;
 	}
-	
+
+	/**
+	 * Returns the username of the currently logged in player.
+	 * 
+	 * @return username
+	 */
 	private String getUserName() {
 		loadInfoFromFile();
 		return userName;
 	}
-	
+
+	/**
+	 * Returns the encrypted password of the currently logged in player.
+	 * 
+	 * @return
+	 */
 	private String getPassword() {
 		loadInfoFromFile();
 		return password;
 	}
+
+	/**
+	 * Loads the username and the password from a textfile, if there isnt any,
+	 * sets loggedIn to false.
+	 */
 	private void loadInfoFromFile() {
 
 		try {
-		    BufferedReader br = new BufferedReader(new FileReader(credentials));
-		    userName = br.readLine();
-		    password = br.readLine();
-		    loggedIn = true;
-		    br.close();
-		}	
-		catch (IOException e) {
+			BufferedReader br = new BufferedReader(new FileReader(credentials));
+			userName = br.readLine();
+			password = br.readLine();
+			loggedIn = true;
+			br.close();
+		} catch (IOException e) {
 			loggedIn = false;
-		}
-		catch (NullPointerException e){
+		} catch (NullPointerException e) {
 			loggedIn = false;
 		}
 
-		
 	}
+
+	/**
+	 * Returns if the user is currently logged in.
+	 * 
+	 * @return
+	 */
 	public boolean isLoggedIn() {
 		return loggedIn;
 	}
-	
+
+	/**
+	 * This method checks the username and password with the database and checks
+	 * if you should be able to log in, then writes a textfile with username and
+	 * password.
+	 * 
+	 * @param userName
+	 * @param password
+	 * @return
+	 */
 	private boolean login(String userName, String password) {
 		boolean suc = false;
 		suc = DbHandler.getInstance().login(userName, password);
-		if(suc) {
+		if (suc) {
 			BufferedWriter out;
 			try {
 				out = new BufferedWriter(new FileWriter(credentials));
