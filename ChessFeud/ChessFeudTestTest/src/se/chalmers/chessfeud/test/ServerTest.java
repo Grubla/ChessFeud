6f7115662ebad6084e153e8ed725610d95d46d76
twister <simonwalmgren@gmail.com>
2012-10-22 00:46:54 +0200

    started to work on the test for all the methods in the database

---
+++ ChessFeud/ChessFeudTestTest/src/se/chalmers/chessfeud/test/ServerTest.java
@@ -0,0 +1,34 @@
+package se.chalmers.chessfeud.test;
+
+import se.chalmers.chessfeud.constants.DbHandler;
+import android.test.AndroidTestCase;
+
+public class ServerTest extends AndroidTestCase {
+	
+	DbHandler dbh = DbHandler.getInstance();
+	
+	public void testLogin() {
+		assertTrue(dbh.login("grubla", "lol"));
+	}
+	public void testAddUser() {
+		dbh.addUser("foo@bar.com", "test", "testpw");
+		boolean b = dbh.login("test", "testpw");
+		dbh.deleteUser("test");
+		assertTrue(b);
+	}
+	public void testDeleteUser() {
+		dbh.addUser("foo@bar.com","delete","delete");
+		boolean b = dbh.userExists("delete");
+		dbh.deleteUser("delete");
+		assertTrue(b && !dbh.userExists("delete"));
+	}
+	public void testUserExists() {
+		dbh.addUser("foo@bar.com", "exists", "exists");
+		dbh.addUser("hej", "exists2", "exists2");
+		boolean b = dbh.userExists("exists");
+		boolean b2 = dbh.userExists("exists2");
+		dbh.deleteUser("exists");
+		dbh.deleteUser("exists2");
+		assertTrue(b&&b2);
+	}
+}
