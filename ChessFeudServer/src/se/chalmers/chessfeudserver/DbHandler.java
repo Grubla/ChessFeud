1a06442587e985fff791d53b7834177182e433e2
twister <simonwalmgren@gmail.com>
2012-10-22 23:15:51 +0200

    formatted and cleaned up

--- ChessFeudServer/src/se/chalmers/chessfeudserver/DbHandler.java
+++ ChessFeudServer/src/se/chalmers/chessfeudserver/DbHandler.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation for handling request from the andriod-app.
  * 
- *@author Simon Almgren 
+ * @author Simon Almgren
  * 
  *         Copyright (c) 2012 Simon Almgren
  */
@@ -90,9 +90,11 @@ public class DbHandler extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(final HttpServletRequest request,
-			final HttpServletResponse response) throws ServletException, IOException {
+			final HttpServletResponse response) throws ServletException,
+			IOException {
 		final int intrequest = (Integer) tags.get(request.getParameter("tag"));
-		final HashMap<String, String[]> h =(HashMap<String, String[]>) request.getParameterMap();
+		final HashMap<String, String[]> h = (HashMap<String, String[]>) request
+				.getParameterMap();
 		final AsyncContext async = request.startAsync();
 		async.setTimeout(TimeUnit.MINUTES.toMillis(5));
 		async.start(new Runnable() {
@@ -102,23 +104,26 @@ public class DbHandler extends HttpServlet {
 			}
 		});
 	}
-	private void doRequest(HashMap<String, String[]> h, int request, HttpServletResponse response){
+
+	private void doRequest(HashMap<String, String[]> h, int request,
+			HttpServletResponse response) {
 
 		try {
 			response.setContentType("text");
 			ServletOutputStream out = response.getOutputStream();
 			switch (request) {
 			case 0:
-				out.print(authenticate(((String[])h.get("username"))[0],
-						((String[])h.get("password"))[0]));
+				out.print(authenticate(((String[]) h.get("username"))[0],
+						((String[]) h.get("password"))[0]));
 				break;
 			case 1:
-				newMove(((String[])h.get("user1"))[0],
-						((String[])h.get("user2"))[0],
-						((String[])h.get("board"))[0]);
+				newMove(((String[]) h.get("user1"))[0],
+						((String[]) h.get("user2"))[0],
+						((String[]) h.get("board"))[0]);
 				break;
 			case 2:
-				List<String> games = getGamesInProgress(((String[])h.get("username"))[0]);
+				List<String> games = getGamesInProgress(((String[]) h
+						.get("username"))[0]);
 				StringBuilder sb = new StringBuilder();
 				for (String s : games) {
 					sb.append(s);
@@ -127,40 +132,41 @@ public class DbHandler extends HttpServlet {
 				out.print(sb.toString());
 				break;
 			case 3:
-				out.print(getStatistics(((String[])h.get("username"))[0]));
+				out.print(getStatistics(((String[]) h.get("username"))[0]));
 				break;
 			case 4:
-				out.print(addUser(((String[])h.get("email"))[0],
-						((String[])h.get("username"))[0],
-						((String[])h.get("password"))[0]));
+				out.print(addUser(((String[]) h.get("email"))[0],
+						((String[]) h.get("username"))[0],
+						((String[]) h.get("password"))[0]));
 				break;
 			case 5:
-				incWins(((String[])h.get("username"))[0]);
+				incWins(((String[]) h.get("username"))[0]);
 				break;
 			case 6:
-				incLosses(((String[])h.get("username"))[0]);
+				incLosses(((String[]) h.get("username"))[0]);
 				break;
 			case 7:
-				incDraws(((String[])h.get("username"))[0]);
+				incDraws(((String[]) h.get("username"))[0]);
 				break;
 			case 8:
-				out.print(newGame(((String[])h.get("user1"))[0],
-						((String[])h.get("user2"))[0],
-						((String[])h.get("board"))[0]));
+				out.print(newGame(((String[]) h.get("user1"))[0],
+						((String[]) h.get("user2"))[0],
+						((String[]) h.get("board"))[0]));
 				break;
 			case 9:
-				deleteGame(((String[])h.get("user1"))[0],
-						((String[])h.get("user2"))[0]);
+				deleteGame(((String[]) h.get("user1"))[0],
+						((String[]) h.get("user2"))[0]);
 				break;
 			case 10:
-				newInquirie(((String[])h.get("user"))[0],
-						((String[])h.get("target"))[0]);
+				newInquirie(((String[]) h.get("user"))[0],
+						((String[]) h.get("target"))[0]);
 				break;
 			case 11:
-				out.print(userExists(((String[])h.get("username"))[0]));
+				out.print(userExists(((String[]) h.get("username"))[0]));
 				break;
 			case 12:
-				List<String> finishedGames = getFinishedGames(((String[])h.get("username"))[0]);
+				List<String> finishedGames = getFinishedGames(((String[]) h
+						.get("username"))[0]);
 				StringBuilder strbuilder = new StringBuilder();
 				for (String s : finishedGames) {
 					strbuilder.append(s);
@@ -169,23 +175,28 @@ public class DbHandler extends HttpServlet {
 				out.print(strbuilder.toString());
 				break;
 			case 13:
-				setGameFinished(((String[])h.get("user1"))[0],
-						((String[])h.get("user2"))[0]);
+				setGameFinished(((String[]) h.get("user1"))[0],
+						((String[]) h.get("user2"))[0]);
 				break;
 			case 14:
-				out.print(deleteUser(((String[])h.get("username"))[0]));
+				out.print(deleteUser(((String[]) h.get("username"))[0]));
 			}
 
 		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			try {
+				response.getOutputStream().print(false);
+			} catch (IOException e1) {}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			try {
+				response.getOutputStream().print(false);
+			} catch (IOException e1) {}
 		} catch (IOException e) {
-			e.printStackTrace();
+			try {
+				response.getOutputStream().print(false);
+			} catch (IOException e1) {}
 		}
 	}
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -194,22 +205,27 @@ public class DbHandler extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		this.doGet(request, response);
 	}
+
 	/**
-	 * Checks if the user exists in the database and the two passwordhashes match.
-	 * @param userName the player logging in
-	 * @param password the password hash.
+	 * Checks if the user exists in the database and the two passwordhashes
+	 * match.
+	 * 
+	 * @param userName
+	 *            the player logging in
+	 * @param password
+	 *            the password hash.
 	 * @return a boolean true if login sucseeded or false otherwise.
 	 * @throws SQLException
 	 */
 	private boolean authenticate(String userName, String password)
 			throws SQLException {
-		if(userExists(userName)) {
+		if (userExists(userName)) {
 			rs = s.executeQuery("select password from auth where userName='"
 					+ userName + "'");
 			rs.first();
 			if (rs.getString(1).equals(password)) {
 				return true;
-			}			 			
+			}
 		}
 		return false;
 	}
@@ -225,19 +241,22 @@ public class DbHandler extends HttpServlet {
 		rs = s.executeQuery("select count(*) from auth where userName='"
 				+ userName + "'");
 		rs.first();
-		if(rs.getInt(1)>0) {
+		if (rs.getInt(1) > 0) {
 			return true;
 		}
 		return false;
 	}
+
 	/**
 	 * Deletes a user from the database, returns false if it didnt work.
+	 * 
 	 * @param userName
 	 * @throws SQLException
 	 */
 	private boolean deleteUser(String userName) throws SQLException {
-		s.executeUpdate("delete from auth where userName='"+userName+"'");
-		s.executeUpdate("delete from statistics where userName='" + userName +"'");
+		s.executeUpdate("delete from auth where userName='" + userName + "'");
+		s.executeUpdate("delete from statistics where userName='" + userName
+				+ "'");
 		return !userExists(userName);
 	}
 
@@ -254,7 +273,6 @@ public class DbHandler extends HttpServlet {
 		List<String> games = new ArrayList<String>();
 		rs = s.executeQuery("select * from game where user1='" + userName
 				+ "' or user2='" + userName + "'");
-		rs.first();
 		while (rs.next()) {
 			games.add(rs.getString(1) + "/" + rs.getString(2) + "/"
 					+ rs.getString(3) + "/" + rs.getString(4) + "/"
@@ -276,7 +294,6 @@ public class DbHandler extends HttpServlet {
 		List<String> games = new ArrayList<String>();
 		rs = s.executeQuery("select * from finishedgames where user1='"
 				+ userName + "' or user2='" + userName + "'");
-		rs.first();
 		while (rs.next()) {
 			games.add(rs.getString(1) + "/" + rs.getString(2) + "/"
 					+ rs.getString(3) + "/" + rs.getString(4) + "/"
@@ -284,25 +301,41 @@ public class DbHandler extends HttpServlet {
 		}
 		return games;
 	}
+
 	/**
 	 * Contacts the database and removes all outdated games.
+	 * 
 	 * @throws SQLException
 	 */
 	private void removeOutdatedGames() throws SQLException {
-		s.executeUpdate("delete from finishedgames where timestamp < (NOW() - 2 DAY)");	
+		s.executeUpdate("delete from finishedgames where timestamp < (NOW() - INTERVAL 3 DAY)");
 	}
+
 	/**
 	 * Moves a game from the game-database to the finishedgames-db.
-	 * @param user1 one of the players.
-	 * @param user2 the other player.
+	 * 
+	 * @param user1
+	 *            one of the players.
+	 * @param user2
+	 *            the other player.
 	 * @throws SQLException
 	 */
-	private void setGameFinished(String user1, String user2) throws SQLException {
-		rs = s.executeQuery("select * from game where (user1='"+user1+"' and user2='"+user2+"') or (user1='"+user2+"' and user2='"+user1+"')");
+	private void setGameFinished(String user1, String user2)
+			throws SQLException {
+		rs = s.executeQuery("select * from game where (user1='" + user1
+				+ "' and user2='" + user2 + "') or (user1='" + user2
+				+ "' and user2='" + user1 + "')");
 		rs.first();
-		s.executeUpdate("insert into finishedgames(user1,user2,board,turns) values('"+rs.getString(1)+"','"+rs.getString(2)+"','"+rs.getString(3)+"','"+rs.getString(4)+"')");
-		s.executeUpdate("delete from game where user1='"+user1+"' and user2='"+user2+"' or user1='"+user2+"' and user2='"+user1+"'");
-		
+		s.executeUpdate("insert into finishedgames(user1,user2,board,turns) values('"
+				+ rs.getString(1)
+				+ "','"
+				+ rs.getString(2)
+				+ "','"
+				+ rs.getString(3) + "','" + rs.getString(4) + "')");
+		s.executeUpdate("delete from game where user1='" + user1
+				+ "' and user2='" + user2 + "' or user1='" + user2
+				+ "' and user2='" + user1 + "'");
+
 	}
 
 	/**
@@ -330,10 +363,11 @@ public class DbHandler extends HttpServlet {
 	 */
 	private boolean addUser(String email, String userName, String password)
 			throws SQLException {
-		if(!userExists(userName)) {
+		if (!userExists(userName)) {
 			s.executeUpdate("insert into auth(email, username, password) values('"
 					+ email + "','" + userName + "','" + password + "')");
-			s.executeUpdate("insert into statistics(username, wld, moves) values('"+userName+"','0/0/0','0')");
+			s.executeUpdate("insert into statistics(username, wld, moves) values('"
+					+ userName + "','0/0/0','0')");
 			return true;
 		}
 		return false;
@@ -351,8 +385,8 @@ public class DbHandler extends HttpServlet {
 		rs.first();
 		String[] wld = rs.getString(1).split("/");
 		int w = Integer.parseInt(wld[0]) + 1;
-		s.executeUpdate("update statistics set wld='" + w +"/"+ wld[1]+ "/" + wld[2]
-				+ "' where username='" + userName + "'");
+		s.executeUpdate("update statistics set wld='" + w + "/" + wld[1] + "/"
+				+ wld[2] + "' where username='" + userName + "'");
 	}
 
 	/**
@@ -367,8 +401,8 @@ public class DbHandler extends HttpServlet {
 		rs.first();
 		String[] wld = rs.getString(1).split("/");
 		int l = Integer.parseInt(wld[1]) + 1;
-		s.executeUpdate("update statistics set wld='" + wld[0] + "/" + l + "/" + wld[2]
-				+ "' where username='" + userName + "'");
+		s.executeUpdate("update statistics set wld='" + wld[0] + "/" + l + "/"
+				+ wld[2] + "' where username='" + userName + "'");
 	}
 
 	/**
@@ -383,8 +417,8 @@ public class DbHandler extends HttpServlet {
 		rs.first();
 		String[] wld = rs.getString(1).split("/");
 		int d = Integer.parseInt(wld[2]) + 1;
-		s.executeUpdate("update statistics set wld='" + wld[0]+"/" + wld[1]+ "/" + d
-				+ "' where username='" + userName + "'");
+		s.executeUpdate("update statistics set wld='" + wld[0] + "/" + wld[1]
+				+ "/" + d + "' where username='" + userName + "'");
 	}
 
 	/**
@@ -397,15 +431,14 @@ public class DbHandler extends HttpServlet {
 	 */
 	private boolean newGame(String user1, String user2, String gameBoard)
 			throws SQLException {
-		if(userExists(user2)) {
+		if (userExists(user2)) {
 			s.executeUpdate("insert into game(user1, user2, board, turns) values('"
 					+ user1
 					+ "','"
 					+ user2
 					+ "','"
 					+ gameBoard.split("/")[0]
-					+ "','"
-					+ "0')");
+					+ "','" + "0')");
 			return true;
 		}
 		return false;
@@ -424,14 +457,16 @@ public class DbHandler extends HttpServlet {
 		String[] boardandturns = newModel.split("/");
 		String newBoard = boardandturns[0];
 		int turns = Integer.parseInt(boardandturns[1]);
-		rs = s.executeQuery("select moves from statistics where username='"+user1+"'");
+		rs = s.executeQuery("select moves from statistics where username='"
+				+ user1 + "'");
 		rs.first();
 		int moves = rs.getInt(1);
-		s.executeUpdate("update statistics set moves='"+(moves+1)+"' where username='"+user1+"'");
-		s.executeUpdate("update game set board='" + newBoard + "' where user1='"
-				+ user1 + "' and user2='" + user2 + "'");
-		s.executeUpdate("update game set board='" + newBoard + "' where user2='"
-				+ user1 + "' and user2='" + user1 + "'");
+		s.executeUpdate("update statistics set moves='" + (moves + 1)
+				+ "' where username='" + user1 + "'");
+		s.executeUpdate("update game set board='" + newBoard
+				+ "' where user1='" + user1 + "' and user2='" + user2 + "'");
+		s.executeUpdate("update game set board='" + newBoard
+				+ "' where user2='" + user1 + "' and user2='" + user1 + "'");
 		s.executeUpdate("update game set turns='" + turns + "' where user1='"
 				+ user1 + "' and user2='" + user2 + "'");
 		s.executeUpdate("update game set turns='" + turns + "' where user2='"
