30df8f93f6e145b6ba2c4ffc5c7b9df7eec7dd4d
twister <simonwalmgren@gmail.com>
2012-10-22 23:20:05 +0200

    cleaned up and added some javadoc

--- ChessFeud/src/se/chalmers/chessfeud/StatsActivity.java
+++ ChessFeud/src/se/chalmers/chessfeud/StatsActivity.java
@@ -2,12 +2,18 @@ package se.chalmers.chessfeud;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+ * Is the class for showing all the stats.
+ * 
+ * @author Sean Pavlov
+ * 
+ *         Copyright (c) Sean Pavlov 2012
+ * 
+ */
 public class StatsActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stats);
 	}
